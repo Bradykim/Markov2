8519602b7bbcaaac0f77b131fdc3a8f99b80bb51
Brady Kim <bnk12@duke.edu>
2020-09-21 12:54:05 -0400

    EfficientMarkov and EfficientWordMarkov Final

--- src/EfficientMarkov.java
+++ src/EfficientMarkov.java
@@ -2,16 +2,31 @@ import java.util.*;
 
 public class EfficientMarkov extends BaseMarkov {
 	private Map<String,ArrayList<String>> myMap;
-	
+
+	/**
+	 * Default constructor has order 3
+	 */
 	public EfficientMarkov(){
 		this(3);
 	}
 
+
+	/**
+	 * Construct an EfficientMarkov object with the given order that is passed though the super.
+	 * Also sets myMap to an empty map.
+	 * @param order size of this markov generator
+	 */
 	public EfficientMarkov(int order) {
 		super(order);
 		myMap = new HashMap<>();
 	}
 
+
+	/**
+	 * Generating keys of Strings whose length are based on the value of myOrder. Also these keys
+	 * are stored in the map and have potential values with them.
+	 * @param text is the String that needs to be evaluated for keys.
+	 */
 	@Override
 	public void setTraining(String text)
 	{
@@ -38,6 +53,12 @@ public class EfficientMarkov extends BaseMarkov {
 
 
 	}
+
+	/**
+	 * Return the value at the given key if there is one.
+	 * @param key is the key that's value we are finding.
+	 * @return the value of the String key.
+	 */
 	@Override
 	public ArrayList<String> getFollows(String key)
 	{
--- src/EfficientWordMarkov.java
+++ src/EfficientWordMarkov.java
@@ -7,15 +7,29 @@ public class EfficientWordMarkov extends BaseWordMarkov
 {
     private Map<WordGram, ArrayList<String>> myMap;
 
+    /**
+     * Default constructor has order 3
+     */
     public EfficientWordMarkov(){
         this(3);
     }
 
+
+    /**
+     * Construct an EfficientWordMarkov object with the given order that is passed though the super.
+     * Also sets myMap to an empty map.
+     * @param order size of this markov generator
+     */
     public EfficientWordMarkov(int order) {
         super(order);
         myMap = new HashMap<>();
     }
 
+    /**
+     * Generating keys of WordGrams whose length are based on the value of myOrder. Also these keys
+     * are stored in the map and have potential values with them.
+     * @param text is the String that needs to be evaluated for keys.
+     */
     @Override
     public void setTraining(String text)
     {
@@ -42,6 +56,12 @@ public class EfficientWordMarkov extends BaseWordMarkov
 
 
     }
+
+    /**
+     * Return the value at the given key if there is one.
+     * @param key is the key that's value we are finding.
+     * @return the value of the String key.
+     */
     @Override
     public ArrayList<String> getFollows(WordGram key)
     {
